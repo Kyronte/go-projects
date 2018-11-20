cf74659b548ea00a75a1e470db407d0037f3e50b
Matias B <kyronte@gmail.com>
2018-11-20 13:37:48 -0300

    Removing old folders, renaming new ones

---
+++ interfaces/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	s := square{sideLength: 10}
+	t := triangle{base: 10, height: 10}
+
+	printArea(s)
+	printArea(t)
+}
+
+func printArea(sh shape) {
+	fmt.Println(sh.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
